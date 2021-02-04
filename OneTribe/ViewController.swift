b305337f9b61c0856e0c95bb9c4b186f48a79e9c
QuintinAdam <quintinjadam@gmail.com>
2021-02-04 11:50:44 -0600

    Initial Commit

---
+++ OneTribe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneTribe
+//
+//  Created by Quintin Adam on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
